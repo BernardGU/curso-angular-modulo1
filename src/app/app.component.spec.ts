bf7444a6b624d923c0ae2ca23fecc425a22116ce
BernardGU <bernardoc.gutierrezc@gmail.com>
2020-06-04 21:42:06 -0500

    Changed name of the app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'curso-angular'`, () => {
+  it(`should have as title 'angular-wishlist'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('curso-angular');
+    expect(app.title).toEqual('angular-wishlist');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('curso-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-wishlist app is running!');
   });
 });
